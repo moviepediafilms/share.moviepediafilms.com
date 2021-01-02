4a2f49c104f20087815455989288bbb3f16a4891
Zeeshan Khan <zkhan1093@gmail.com>
2021-01-03 00:40:37 +0530

    adding base url

--- share/views/movie.py
+++ share/views/movie.py
@@ -7,4 +7,4 @@ app = Blueprint("movie_app", __name__, url_prefix="/movie")
 @app.route("/<int:movie_id>/<string:slug>/")
 def movie_detail(movie_id, slug):
     movie = Movie.query.filter_by(id=movie_id).first_or_404()
-    return render_template("movie.html", title="Welcome to flask", movie=movie)
+    return render_template("movie.html", movie=movie)
