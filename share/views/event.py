53b5ca888fd90e5df2fb4899076577ac424d24a2
Zeeshan Khan <zkhan1093@gmail.com>
2024-01-03 00:48:39 -0500

    updated seo for event

--- share/views/event.py
+++ share/views/event.py
@@ -8,12 +8,12 @@ app = Blueprint("event_app", __name__, url_prefix="/events")
 def movie_detail(slug):
     events = {
         "iim-rohtak-2024": {
-            "title": "IIM Rohtak 2024",
+            "title": "Moviepedia at IIM Rohtak | The Shorts Film Festival 2024 | Submit Your Film Now",
             "keywords": "IIM Rohtak 2024, IIM Rohtak, Moviepedia, Moviepediafilms, Moviepedia 2024",
-            "about": "IIM Rohtak 2024 is a movie event organized by Moviepediafilms. It is a movie event for the students across the country.",
-            "image": "img/events/iim-rohtak-2024.png",
+            "about": "Are you an aspiring filmmaker looking to showcase your creativity and talent? IIM Rohtak, in collaboration with Moviepedia, presents an incredible opportunity for all budding filmmakers. Get your film screened and stand a chance to win cash prizes worth 40,000 INR!",
+            "image": "img/events/iim-rohtak-2024-alt.png",
             "slug": slug,
-            "poster": "img/events/iim-rohtak-2024.png",
+            "poster": "img/events/iim-rohtak-2024-alt.png",
         }
     }
     meta = events.get(slug, None)
